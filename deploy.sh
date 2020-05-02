afdf29112f6ab7cd4dd91f8f3e62667a567e5e11
EC2 Default User <ec2-user@ip-172-31-31-166.us-east-2.compute.internal>
2020-05-02 14:43:07 +0000

    4

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ fi
 i=0 p=0 b=0 d=0
 
 CF_FILE="/tmp/cf_file.txt"
-DEPLOYMENTS_BUCKET="maudeployments"
+DEPLOYMENTS_BUCKET="my-unique-s3-bucket-name-8952188"
 
 case "$1" in
   -i|--install)
@@ -59,7 +59,7 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --template-file $CF_FILE \
   --parameter-overrides Project=cf_lab2  \
-  --stack-name "my-awesome-stack3" \
+  --stack-name "my-awesome-stack5" \
   --capabilities CAPABILITY_NAMED_IAM
 fi
 
